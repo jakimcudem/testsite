0f8121cc40c55b4b7e7e5b303122acba7532f7f5
jakimcudem <48454827+jakimcudem@users.noreply.github.com>
2022-10-10 20:59:42 +0100

    what

---
+++ bruh.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Filter the mail content type.
+ */
+function wpdocs_set_html_mail_content_type() {
+    return 'text/html';
+}
+add_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
+
+$message = '<p>Nombre:' . $_POST['nombre'] . '</p><p>Email:' . $_POST['email'] .'</p><p>...</p>';
+$result = wp_mail('proyectosonline@isern.com', 'New mail', $message);
+if ($result ) {
+    // Do something here
+}
+remove_filter( 'wp_mail_content_type', 'wpdocs_set_html_mail_content_type' );
+?>
